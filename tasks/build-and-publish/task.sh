a4581f900142ca1b86edb99e87e980ccf799b6ce
Simon Kornweih <s.kornweih@ostfalia.de>
2020-08-25 17:06:10 +0200

    update

--- tasks/build-and-publish/task.sh
+++ tasks/build-and-publish/task.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "YES"
-ls concourse-git
+IMAGE_NAME=$1 # Parameter 1                                                                                                                                                                                                               
+IMAGE_TAG=${2:-latest} # Parameter 2 (if set, else use default: 'latest')                                                                                                                                                                                                                 echo "Building Image $IMAGE_NAME with tag $IMAGE_TAG"
+echo "Building Image $IMAGE_NAME with tag $IMAGE_TAG"
+docker build -t 192.168.178.53:80/bachelor2020/$IMAGE_NAME:$IMAGE_TAG /home/ba-admin/git/BA-Scripte/
+sudo docker login 192.168.178.53:80
+docker tag $IMAGE_NAME 192.168.178.53:80/bachelor2020/$IMAGE_NAME:$IMAGE_TAG
+docker push 192.168.178.53:80/bachelor2020/$IMAGE_NAME:$IMAGE_TAG
