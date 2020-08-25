d08c4929436213f25859542506b3a71351053dd0
Simon Kornweih <s.kornweih@ostfalia.de>
2020-08-25 19:25:58 +0200

    update

--- tasks/deploy-image/task.sh
+++ tasks/deploy-image/task.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+kubectl config --kubeconfig=concourse-git/kubectl-YAML/config
+kubectl create secret docker-registry harborregcred --docker-server='192.168.178.53:80' --docker-username='admin' --docker-password='Harbor12345' --docker-email='simon.kornweih@mialbox.org'
 kubectl apply -f concourse-git/kubectl-YAML/deployment.yml
 
