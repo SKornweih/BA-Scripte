44630ff6a815b88426eb085ea72b85faedaa9ae4
Simon Kornweih <s.kornweih@ostfalia.de>
2020-08-26 13:37:22 +0200

    update

--- tasks/deploy-image/task.sh
+++ tasks/deploy-image/task.sh
@@ -3,5 +3,6 @@ kubectl config set-credentials minikube/kubernetes --username=minikube --passwor
 kubectl config set-cluster kubernetes  --insecure-skip-tls-verify=true --server=172.17.0.2:8443
 kubectl config set-context default/kubernetes/minikube  --user=minikube/kubernetes --namespace=default --cluster=kubernetes
 kubectl config use-context default/kubernetes/minikube
+kubectl config view
 kubectl apply -f concourse-git/kubectl-YAML/deployment.yml
 
