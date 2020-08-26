ba4560ec25551e9e57240387c0ea9f0e3b2bcb05
Simon Kornweih <s.kornweih@ostfalia.de>
2020-08-26 10:33:50 +0200

    update

--- tasks/deploy-image/task.sh
+++ tasks/deploy-image/task.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
+kubectl create secret docker-registry harborregcred --docker-server='192.168.178.53:80' --docker-username='admin' --docker-password='Harbor12345' --docker-email='simon.kornweih@mialbox.org'
 kubectl config set-credentials minikube/kubernetes --username=minikube --password=minikube
 kubectl config set-cluster kubernetes  --insecure-skip-tls-verify=true --server=192.168.178.53
 kubectl config set-context default/kubernetes/minikube  --user=minikube/kubernetes --namespace=default --cluster=kubernetes
 kubectl config use-context default/kubernetes/minikube
-kubectl create secret docker-registry harborregcred --docker-server='192.168.178.53:80' --docker-username='admin' --docker-password='Harbor12345' --docker-email='simon.kornweih@mialbox.org'
 kubectl apply -f concourse-git/kubectl-YAML/deployment.yml
 
