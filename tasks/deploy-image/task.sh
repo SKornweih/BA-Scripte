748cec8021a12a341702801bc5918a6239821305
Simon Kornweih <s.kornweih@ostfalia.de>
2020-08-27 13:48:29 +0200

    update

--- tasks/deploy-image/task.sh
+++ tasks/deploy-image/task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p $HOME/.kube
-sudo cp -i concourse-git/kubectl-YAML/admin.conf $HOME/.kube/config -y
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+cp -i concourse-git/kubectl-YAML/admin.conf $HOME/.kube/config -y
+chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl config view
 kubectl apply -f concourse-git/kubectl-YAML/deployment.yml
