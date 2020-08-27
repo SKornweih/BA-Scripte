8b7eaf91ec9185193523f83530320b2d502a08d0
Simon Kornweih <s.kornweih@ostfalia.de>
2020-08-27 11:05:14 +0200

    update

--- tasks/deploy-image/task.sh
+++ tasks/deploy-image/task.sh
@@ -1,10 +1,8 @@
 
 #!/bin/bash
-
 kubectl config set-credentials kubernetes --username=kubernetes-admin --password=kubernetes-admin
 kubectl config set-cluster kubernetes  --insecure-skip-tls-verify=true --server=https://192.168.178.53:6443
 kubectl config set-context default/kubernetes  --user=kubernetes --namespace=default --cluster=kubernetes
 kubectl config use-context default/kubernetes
 kubectl config view
 kubectl apply -f concourse-git/kubectl-YAML/deployment.yml
-
