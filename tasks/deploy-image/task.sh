1b57af3b7acd1921e7c1b37ad6d4bfbf34698bae
Simon Kornweih <s.kornweih@ostfalia.de>
2020-08-27 11:28:17 +0200

    update

--- tasks/deploy-image/task.sh
+++ tasks/deploy-image/task.sh
@@ -1,5 +1,5 @@
-
 #!/bin/bash
+
 kubectl config set-credentials kubernetes --username=kubernetes-admin --password=kubernetes-admin
 kubectl config set-cluster kubernetes  --insecure-skip-tls-verify=true --server=https://192.168.178.53:6443
 kubectl config set-context default/kubernetes  --user=kubernetes --namespace=default --cluster=kubernetes
