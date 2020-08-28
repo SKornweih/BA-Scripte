f7106c2086746cc6932abbceff2e23354705cf62
SKornweih <simon.kornweih@mailbox.org>
2020-08-28 14:12:00 +0200

    update

--- tasks/check-security-in/task.sh
+++ tasks/check-security-in/task.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl -ku "admin:Harbor12345" --header "Content-Type: application/json" -X POST "https://192.168.178.58/api/v2.0/projects/security-in/repositories/webserver/artifacts/latest/scan"
-curl -ku "admin:Harbor12345" --header "Content-Type: application/json" -X GET "https://192.168.178.58/api/v2.0/projects/security-in/repositories/webserver/artifacts/" | jq '.[].addition_links.vulnerabilities.href' -r >> tests.txt
-head -n $i tests.txt | tail -n 1
-curl -ku "admin:Harbor12345" --header "Content-Type: application/json" -X GET "https://192.168.178.58/" + $1 | jq '.'
+curl -sku "admin:Harbor12345" --header "Content-Type: application/json" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -X POST "https://192.168.178.58/api/v2.0/projects/security-in/repositories/webserver/artifacts/latest/scan"
+VULN_PATH=$(curl -ku "admin:Harbor12345" --header "Content-Type: application/json" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -X GET "https://192.168.178.58/api/v2.0/projects/security-in/repositories/webserver/artifacts/" | jq '.[].addition_links.vulnerabilities.href' -r)
+curl -ku "admin:Harbor12345" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" --header "Content-Type: application/json" -X GET "https://192.168.178.58/$VULN_PATH" | jq '.'
