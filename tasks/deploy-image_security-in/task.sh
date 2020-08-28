8ac5b9abcfe2020793b2dd3f6ab9c7fd1e495942
SKornweih <simon.kornweih@mailbox.org>
2020-08-28 18:27:47 +0200

    update

--- tasks/deploy-image_security-in/task.sh
+++ tasks/deploy-image_security-in/task.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+echo "Setting kubeconfig..."
 mkdir -p $HOME/.kube
 cp -i concourse-git/kubectl-YAML/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl config view
+sleep 3
+echo "Kubeconfig set"
+echo "Apply security-in_deployment with:"
+cat concourse-git/kubectl-YAML/security-in_deployment.yml
 kubectl apply -f concourse-git/kubectl-YAML/security-in_deployment.yml
+echo "security-in_deployment applyed"
