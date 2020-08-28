21b34cdea0687b91869812e00bea8d9b067d445c
SKornweih <simon.kornweih@mailbox.org>
2020-08-28 18:22:05 +0200

    update

--- tasks/check-security-in/task.sh
+++ tasks/check-security-in/task.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 curl -sku "admin:Harbor12345" --header "Content-Type: application/json" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -X POST "https://192.168.178.58/api/v2.0/projects/security-in/repositories/webserver/artifacts/latest/scan"
-echo "Start checking..."
+echo "Start checking last Image..."
 #sleep 10
 VULN_PATH=$(curl -sku "admin:Harbor12345" --header "Content-Type: application/json" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -X GET "https://192.168.178.58/api/v2.0/projects/security-in/repositories/webserver/artifacts/" | jq '.[].addition_links.vulnerabilities.href' -r)
 RES=$(curl -sku "admin:Harbor12345" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" --header "Content-Type: application/json" -X GET "https://192.168.178.58$VULN_PATH")
 while true; do
 
   if [[ $RES == "{}" || $RES == "" ]]; then
-    echo "Waiting for result..."
+    echo "Check complete!"
+    echo "Waiting for result of Image..."
     sleep 10
     curl -sku "admin:Harbor12345" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" --header "Content-Type: application/json" -X GET "https://192.168.178.58$VULN_PATH" | jq '.' -r  >> res.txt
     severity=$(grep -a -m 1 -h -r 'severity' res.txt)
     if [[ $severity == 'severity": "Critical",' ]]; then
-      echo "failed the clair test with" $severity 
+      echo "Image failed the Clair-Test with" $severity 
       exit 1
     else    
-      echo "passed the clair test with" $severity   
+      echo "Image passed the Clair-Test with" $severity   
       exit 0
     fi
 echo $severity
