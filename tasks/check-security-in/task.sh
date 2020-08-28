9eb44fff9979fb3a0a7b4fd042f1d2c8f7fcc1ed
SKornweih <simon.kornweih@mailbox.org>
2020-08-28 17:31:58 +0200

    update

--- tasks/check-security-in/task.sh
+++ tasks/check-security-in/task.sh
@@ -8,9 +8,19 @@ while true; do
   if [[ $RES == "{}" || $RES == "" ]]; then
     echo "Waiting for result..."
     sleep 10
-    RES=$(curl -sku "admin:Harbor12345" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" --header "Content-Type: application/json" -X GET "https://192.168.178.58$VULN_PATH")
+    curl -sku "admin:Harbor12345" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" --header "Content-Type: application/json" -X GET "https://192.168.178.58$VULN_PATH" | jq '.' -r  >> res.txt
+    severity=$(grep -a -m 1 -h -r 'severity' res.txt)
+    if [[ $severity == 'severity": "Critical",' ]]; then
+      echo "failed the clair test"
+      exit 1
+    else    
+      echo "passed the clair test" 
+      exit 0
+    fi
+echo $severity
+#    RES=$(curl -sku "admin:Harbor12345" -H "Accept: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" --header "Content-Type: application/json" -X GET "https://192.168.178.58$VULN_PATH" ) | jq '.' -r 
   else
-    echo $RES | jq '.'
+    echo $RES | jq '.' -r 
     exit 0
   fi
 
