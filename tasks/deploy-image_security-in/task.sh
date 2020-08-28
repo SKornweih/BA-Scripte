6bb4526814c8828c244976f1dad98469712a0bd4
SKornweih <simon.kornweih@mailbox.org>
2020-08-28 09:19:59 +0200

    update

--- tasks/deploy-image_security-in/task.sh
+++ tasks/deploy-image_security-in/task.sh
@@ -4,4 +4,4 @@ mkdir -p $HOME/.kube
 cp -i concourse-git/kubectl-YAML/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl config view
-kubectl apply -f concourse-git/kubectl-YAML/deployment.yml
+kubectl apply -f concourse-git/kubectl-YAML/security-in_deployment.yml
