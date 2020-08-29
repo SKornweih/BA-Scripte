ad53551c96365f8cd44b6aebbe23c44737a6e226
SKornweih <simon.kornweih@mailbox.org>
2020-08-29 12:13:35 +0200

    update

--- tasks/check-lab-container-running/task.sh
+++ tasks/check-lab-container-running/task.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-rm pods-status.txt
+
+echo "Setting kubeconfig..."
+mkdir -p $HOME/.kube
+cp -i concourse-git/kubectl-YAML/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+sleep 3
+echo "Kubeconfig set"
+echo "Checking Pod-Status..."
 kubectl get pods -n lab  >> pods-status.txt 
 PODSTATUS=$(grep -o "1/1" pods-status.txt)
 kubectl get pods -n lab
 if [[ $PODSTATUS == "1/1" ]]; then
 echo "Pod Status: Running..."
+rm pods-status.txt
 else
 echo "Pod Status: Not Running..."
+rm pods-status.txt
 exit 1
 fi
