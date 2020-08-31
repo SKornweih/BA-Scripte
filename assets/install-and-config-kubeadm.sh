669dbfdd1e009e68b8bdb80f4c0a2c87d736c197
SKornweih <50403681+SKornweih@users.noreply.github.com>
2020-08-31 11:47:56 +0200

    Update install-and-config-kubeadm.sh

--- assets/install-and-config-kubeadm.sh
+++ assets/install-and-config-kubeadm.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 sudo swapoff -a  	  
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.178.53
 mkdir -p $HOME/.kube
